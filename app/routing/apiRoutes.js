60ff8356087665a9673d40a0b4b4ba5f8f6fe66a
Adnan Yousef <yousef.eddy@gmail.com>
2018-08-28 18:50:12 -0500

    Updated apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,28 @@ function apiRoutes(app) {
     });
 
     app.post('/api/friends', function(req,res){
-        friends.push(req.body);
-        res.json(friends);
+        // Create newFriend object
+        var newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: []
+        };
+        for (var i=0; i < req.body.scores.length; i++) {
+            newFriend.scores[i] = parseInt(req.body.scores[i]);
+        };
+
+        var match = {
+            name: 'Testing this app',
+            photo: 'https://assets.nobelprize.org/images/einstein-12923-content-portrait-mobile-tiny.jpg',
+            scores: [3,3,3,3,3,3,3,3,3,3]
+        };
+
+
+        // Add new friend to array
+        friends.push(newFriend);
+
+        // Send back the match
+        return res.json(match);
     });
 };
 
