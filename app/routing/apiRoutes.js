ba48b7b749b3a060c14eb6b08a0219fe5d6bca82
Adnan Yousef <yousef.eddy@gmail.com>
2018-08-28 19:23:20 -0500

    Added matching functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -24,22 +24,15 @@ function apiRoutes(app) {
             for (var k=0; k < friends[j].scores.length; k++) {
                 diff += Math.abs(friends[j].scores[k] - newFriend.scores[k]);
             };
-
             scoreDiffs.push(diff);
         };
 
         var matchIndex = scoreDiffs.indexOf(Math.min(...scoreDiffs));
-        console.log(matchIndex);
-
+        var match = friends[matchIndex];
 
         // Add new friend to array
         friends.push(newFriend);
 
-        var match = {
-            name: "Testing",
-            photo: 'https://static.highsnobiety.com/wp-content/uploads/2018/04/18123341/kanye-west-creativity-guide-011-480x320.jpg',
-            scores: [3,3,3,3,3,3,3,3,3,3]
-        };
         // Send back the match
         return res.json(match);
     });
