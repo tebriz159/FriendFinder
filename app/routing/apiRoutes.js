787da7e617108a4e6205b60c9f49bcfb48eeb8b8
Adnan Yousef <yousef.eddy@gmail.com>
2018-08-28 19:08:58 -0500

    Working on finding closest match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,16 +17,32 @@ function apiRoutes(app) {
             newFriend.scores[i] = parseInt(req.body.scores[i]);
         };
 
-        var match = {
-            name: 'Testing this app',
-            photo: 'https://assets.nobelprize.org/images/einstein-12923-content-portrait-mobile-tiny.jpg',
-            scores: [3,3,3,3,3,3,3,3,3,3]
+        // Create array that holds 'index' and 'diff from newFriend'
+        var scoreDiffs = [];
+        for (var j=0; j < friends.length; j++) {
+            var diff = 0;
+            for (var k=0; k < friends[j].scores.length; k++) {
+                diff += Math.abs(friends[j].scores[k] - newFriend.scores[k]);
+            };
+
+            var scoreDiff = {
+                index: j,
+                diff: diff
+            };
+            scoreDiffs.push(scoreDiff);
         };
+
+
 
 
         // Add new friend to array
         friends.push(newFriend);
 
+        var match = {
+            name: "Testing",
+            photo: 'https://static.highsnobiety.com/wp-content/uploads/2018/04/18123341/kanye-west-creativity-guide-011-480x320.jpg',
+            scores: [3,3,3,3,3,3,3,3,3,3]
+        };
         // Send back the match
         return res.json(match);
     });
